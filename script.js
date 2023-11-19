e316a872f0330c665df51588329841669f65b2af
Wizkidojnr1103 <denniskwarteng1103@gmail.com>
2023-11-19 13:43:32 -0800

    update

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Function to toggle between short and full synopses
 function toggleSynopsis(movieId) {
     var shortSynopsis = document.getElementById(movieId).getElementsByClassName('short-synopsis')[0];
     var fullSynopsis = document.getElementById(movieId).getElementsByClassName('full-synopsis')[0];
@@ -8,5 +9,26 @@ function toggleSynopsis(movieId) {
     } else {
         shortSynopsis.style.display = 'none';
         fullSynopsis.style.display = 'block';
+    }
+}
+
+// Function to perform movie search
+function searchMovies() {
+    // Retrieve the search query
+    var searchQuery = document.getElementById('searchQuery').value.toLowerCase();
+
+    // Get all movie sections
+    var movieSections = document.getElementsByClassName('movie');
+
+    // Iterate through movie sections and hide/show based on the search query
+    for (var i = 0; i < movieSections.length; i++) {
+        var movieTitle = movieSections[i].getElementsByTagName('h2')[0].innerText.toLowerCase();
+        var movieGenre = movieSections[i].getElementsByTagName('p')[2].innerText.toLowerCase(); // Assuming genre is at index 2
+
+        if (movieTitle.includes(searchQuery) || movieGenre.includes(searchQuery)) {
+            movieSections[i].style.display = 'block'; // Show matching section
+        } else {
+            movieSections[i].style.display = 'none'; // Hide non-matching section
+        }
     }
 }
