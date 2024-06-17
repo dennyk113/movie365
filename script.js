a5372a5bbdb41c82408858dc8e5643b6e4a9f577
Wizkidojnr1103 <denniskwarteng1103@gmail.com>
2024-06-16 20:54:41 -0700

    done

--- script.js
+++ script.js
@@ -42,3 +42,34 @@ function searchMovies() {
         // You can also update the DOM to display a message on the webpage instead of using an alert.
     }
 }
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("acceptButton");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the page loads, display the modal
+window.onload = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on the accept button, close the modal
+btn.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// If the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
